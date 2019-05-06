0c62bd15dc34230bf56b2dc516e48c20eb323baa
Fariz <fariz.siracli@gmail.com>
2019-05-06 21:50:51 +0400

    modify controller

--- src/main/java/com/fs/spring/demo/controller/MyController.java
+++ src/main/java/com/fs/spring/demo/controller/MyController.java
@@ -2,10 +2,13 @@ package com.fs.spring.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/demo")
 public class MyController {
+
     @GetMapping("/{name}")
     public String greet(@PathVariable String name){
         return "Hello " + name;
